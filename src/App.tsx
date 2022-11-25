8d8ffe8405660bd716d02eba8151128a259e8b7c
Soezdemir23 <soezdemir3423@gmail.com>
2022-11-25 17:21:09 +0100

    implement and test datafetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { consumerKey } from "./env";
+import { Event } from "./eventsInterface";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [events, setEvents] = useState<Event[]| null>(null)
+  useEffect(() => {
+    
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey="+consumerKey, { mode: "cors" })
+      .then(result => result.json())
+      .then(content => setEvents(content._embedded.events))
+  },[])
+  return (<div>
+    <ul>
+      {events === null ? "<li>No events found</li>" :events.map((party) => (
+        <li key={party.id}>
+          {party.name}
+        </li>
+
+      ))}
+    </ul>
+  </div>);
 }
 
 export default App;
