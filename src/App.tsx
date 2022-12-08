1c494a33af374877e27da4bc2eb3f264d38e661e
Serdar Özdemir <soezdemir3423@gmail.com>
2022-12-08 15:35:40 +0100

    tested and reverted feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import "./App.css";
-import { consumerKey } from "./env";
+import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import Product from "./Product";
 import ShoppingPage from "./ShoppingPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/*Homepage, where the stuff is being presented*/}
+
+ 
+   return (
+     <BrowserRouter>
+       <Routes>
+         {/*Homepage, where the stuff is being presented*/}
         <Route path="/" element={<HomePage />} />
-        {/*Page for items. Try to make a switch out of it.*/}
-        <Route path="/products" element={<Product />} />
-        {/*shopping thing */}
-        <Route path="/shopping-cart" element={<ShoppingPage />} />
-        {/*Thank people for buying the stuff */}
-      </Routes>
-    </BrowserRouter>
-  )
+         {/*Page for items. Try to make a switch out of it.*/}
+        <Route path="/products/" element={<Product />} />
+         {/*shopping thing */}
+         <Route path="/shopping-cart" element={<ShoppingPage />} />
+         {/*Thank people for buying the stuff */}
+       </Routes>
+     </BrowserRouter>
+   )
 }
 
 export default App;
