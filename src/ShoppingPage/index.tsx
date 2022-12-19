1549a628c03edd8c74873c2a9335fc04084e60a0
Serdar Özdemir <soezdemir3423@gmail.com>
2022-12-19 13:27:17 +0100

    Update index.tsx
    added interface props for later shoppingcart page

--- src/ShoppingPage/index.tsx
+++ src/ShoppingPage/index.tsx
@@ -1,13 +1,14 @@
+import React from "react";
+import { ShoppingPageProps } from "../eventsInterface";
 import Footer from "../Footer";
 import Header from "../Header";
 
-export default function ShoppingPage() {
+export default function ShoppingPage({onClick}: ShoppingPageProps)   {
     return (
         <>
         <Header />
         <h1>Shopping Page</h1>
         <Footer />
-        
         </>
 
     )
