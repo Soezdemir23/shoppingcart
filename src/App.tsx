08fe2dca2810bf482ca074513472286eed675c38
Serdar Özdemir <soezdemir3423@gmail.com>
2022-12-19 14:55:01 +0100

    implemented immediate buy handling
    The user can buy tickets until the number of tickets is the same as the number of max. tickets for events. Now I need to pass this down to the component and make the button jiggle when the limit is reached.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import "./App.css";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import Product from "./Product";
 import ShoppingPage from "./ShoppingPage";
 import React, { useEffect, useState } from "react";
 import { consumerKey } from "./env";
-import { EmbeddedClass, ValueClass} from "./eventsInterface";
+import { EmbeddedClass, ShoppingCart, ValueClass } from "./eventsInterface";
 
 function App() {
   const [feed, setFeed] = useState<EmbeddedClass | undefined>(undefined);
-  const [shoppingCart, setShoppingCart] = useState(null);
+  const [shoppingCart, setShoppingCart] = useState<ShoppingCart[]>([]);
 
   // I SWEAR last time I made a better looking async await utilization.
   // Insane
@@ -35,25 +34,84 @@ function App() {
     }
   }
 
-  const handleBuySubmission = (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
+  const handleBuySubmission = (e: React.FormEvent<HTMLFormElement>) => {
     const form = e.target as HTMLFormElement;
     const select = form.elements.namedItem("selection") as HTMLSelectElement;
-    console.log(typeof(select.value));
-    
-    
-      };
+    console.log(typeof select.value);
+  };
 
-  // we buy
-  const handleBuyClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    console.log(e.currentTarget.dataset.key)
-  }
+  // we buy one ticket by clicking here, then we need to create the first shoppingList object here or
+  const handleBuyClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+   
+    let product = feed?.events.filter(
+      (event) => event.id === e.currentTarget.dataset.key
+    )[0];
+    // does that product already exist in the state shoppingCart?
+    // if it does, then check if the number of tickets is inequal to the currently reserved tickets
+    //  if it is equal, then the buy one button needs to be shaking and be red, "Max. Tickets reached".
+    //  if not, then increment the currentTickets property by one each time the user clicks
+    // if not, create a new object that has
+    //  name of the event
+    //  id of the event
+    //  max Tickets or 99 Tickets as max Tickets
+    //  currentTickets at 1
+    // set the update into the shoppingcart state array
+
+    // new entry
+    if (
+      shoppingCart?.filter(
+        (productInCart) => productInCart.id === product?.id
+      )[0] === undefined
+    ) {
+      const name = product?.name;
+      const id = product?.id;
+      let maxTickets: number = 0;
+      if (product?.ticketLimit !== undefined) {
+        const regex = /\d+/;
+        const matches = product.ticketLimit.info?.match(regex);
+        let result = "";
+        matches?.forEach((res) => (result += res));
+        maxTickets = parseInt(result);
+      } else {
+        maxTickets = 99;
+      }
+      let object: ShoppingCart = {
+        name: name,
+        id: id,
+        maxTickets: maxTickets,
+        numOfReservedTickets: 1,
+        maxReached: false
+      };
+      setShoppingCart([...shoppingCart, object])
+    } else {
+      // go through the products, check for the 
+      setShoppingCart(shoppingCart.map((prod) => {
+       if(prod.id === product?.id) {
+
+        if (prod.maxReached === false) {
+          prod.numOfReservedTickets +=1;
+        }
+        if (prod.numOfReservedTickets === prod.maxTickets) {
+          prod.maxReached = true;
+        }
+        return prod;
+       }
+       return prod;
+      }))
+    }
+    // first of all, create properties made of: name of event, id and if available, max. Tickets, IF the
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         {/*Homepage, where the stuff is being presented*/}
-        <Route path="/" element={<HomePage feed={feed} onClick={handleBuyClick}/>} />
+        <Route
+          path="/"
+          element={<HomePage feed={feed} onClick={handleBuyClick} />}
+        />
         {/*Page for items. Try to make a switch out of it.*/}
         <Route
           path="/products/:id"
