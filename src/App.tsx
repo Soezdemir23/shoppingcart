6800fbd398879be6bbe1323bcae2f43840295618
Serdar Özdemir <soezdemir3423@gmail.com>
2022-12-19 13:28:36 +0100

    added functions to handle ticket buying
    Added filler functions to handle formElement buying and also button buying for immediate buying.
    Added correct props to be drilled into child components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
-import "./App.css"
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import Product from "./Product";
 import ShoppingPage from "./ShoppingPage";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { consumerKey } from "./env";
-import { EmbeddedClass, ValueClass } from "./eventsInterface";
+import { EmbeddedClass, ValueClass} from "./eventsInterface";
 
 function App() {
-  
   const [feed, setFeed] = useState<EmbeddedClass | undefined>(undefined);
+  const [shoppingCart, setShoppingCart] = useState(null);
 
-  
   // I SWEAR last time I made a better looking async await utilization.
   // Insane
   useEffect(() => {
@@ -25,7 +24,8 @@ function App() {
   async function fetchFeed() {
     try {
       const response = await fetch(
-        "https://app.ticketmaster.com/discovery/v2/events.json?apikey="+consumerKey,
+        "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" +
+          consumerKey,
         { mode: "cors" }
       );
       const data: Promise<ValueClass> = await response.json();
@@ -35,20 +35,39 @@ function App() {
     }
   }
 
- 
-   return (
-     <BrowserRouter>
-       <Routes>
-         {/*Homepage, where the stuff is being presented*/}
-        <Route path="/" element={<HomePage feed={feed} />} />
-         {/*Page for items. Try to make a switch out of it.*/}
-        <Route path="/products/:id" element={<Product feed={feed} />} />
-         {/*shopping thing */}
-         <Route path="/shopping-cart" element={<ShoppingPage />} />
-         {/*Thank people for buying the stuff */}
-       </Routes>
-     </BrowserRouter>
-   )
+  const handleBuySubmission = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    const form = e.target as HTMLFormElement;
+    const select = form.elements.namedItem("selection") as HTMLSelectElement;
+    console.log(typeof(select.value));
+    
+    
+      };
+
+  // we buy
+  const handleBuyClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e.currentTarget.dataset.key)
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/*Homepage, where the stuff is being presented*/}
+        <Route path="/" element={<HomePage feed={feed} onClick={handleBuyClick}/>} />
+        {/*Page for items. Try to make a switch out of it.*/}
+        <Route
+          path="/products/:id"
+          element={<Product feed={feed} onSubmit={handleBuySubmission} />}
+        />
+        {/*shopping thing */}
+        <Route
+          path="/shopping-cart"
+          element={<ShoppingPage onClick={handleBuyClick} />}
+        />
+        {/*Thank people for buying the stuff */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
