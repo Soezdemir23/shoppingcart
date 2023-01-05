29ca8642d752c544807f7a5a3d8653bade103fa5
Serdar Özdemir <soezdemir3423@gmail.com>
2023-01-05 16:25:17 +0100

    update navigation, worked
    add nav link to the other pages, too.

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -2,32 +2,58 @@ import { Link } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
 import { AllProps, EmbeddedClass, ShoppingCart } from "../eventsInterface";
-export default function HomePage(
-  props: {
-    feed: EmbeddedClass | undefined;
-    onClick: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void);
-    shoppingCart: ShoppingCart[],
-    onIncrementClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    onDecrementClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  }
-  ) {
-    const{feed,onClick,onDecrementClick,onIncrementClick,shoppingCart} = props
+export default function HomePage(props: {
+  feed: EmbeddedClass | undefined;
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  shoppingCart: ShoppingCart[];
+  onIncrementClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+  onDecrementClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+  onRemoveClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}) {
+  const {
+    feed,
+    onClick,
+    onDecrementClick,
+    onIncrementClick,
+    shoppingCart,
+    onRemoveClick,
+  } = props;
 
-  const handleBuyClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)  => {
-
+  const handleBuyClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     onClick!(e);
-  }
+  };
   return (
     <div className="flex flex-col">
-      <Header  shoppingCart={shoppingCart} onIncrementClick={onIncrementClick} onDecrementClick={onDecrementClick} />
+      <Header
+        shoppingCart={shoppingCart}
+        onIncrementClick={onIncrementClick}
+        onDecrementClick={onDecrementClick}
+        onRemoveClick={(e) => onRemoveClick(e)}
+      />
       <main>
-        <h3 className="bg-emerald-500 text-center pt-5 text-2xl text-rose-900">
-          Currently available events
-        </h3>
-        <section className="flex flex-col gap-4">
+        <nav className="hidden xl:py-4 xl:bg-blue-600 xl:flex xl:justify-center">
+          <ul className="flex gap-5 relative top-3">
+            <li className=" text-5xl text-white cursor-pointer active:text-orange-400">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="text-5xl text-white cursor-pointer active:text-orange-400">
+              <Link to="/products">Products</Link>
+            </li>
+            <li className="text-5xl text-white cursor-pointer active:text-orange-400">
+              <Link to="/shopping-cart">Shopping Cart</Link>
+            </li>
+          </ul>
+        </nav>
+        <section className="flex flex-col gap-4 xl:grid xl:grid-cols-2">
           {feed?.events.map((event) => (
             <article
-              className="py-3 px-3 flex flex-col justify-center content-center border-2"
+              className="py-3 px-3 flex flex-col justify-center content-center border-2 "
               key={event.id}
             >
               {/* title card */}
@@ -35,7 +61,11 @@ export default function HomePage(
                 <h6 className="text-center">{event.name}</h6>
                 <img
                   className="center"
-                  src={event.images.filter(img => img.width === 2048 && img.ratio === "16_9")[0].url }
+                  src={
+                    event.images.filter(
+                      (img) => img.width === 2048 && img.ratio === "16_9"
+                    )[0].url
+                  }
                   alt="event"
                 ></img>
               </div>
@@ -44,19 +74,33 @@ export default function HomePage(
                 if the shoppingCart id or content is undefined, or returns nothing, then 
                     
                 */}
-                {shoppingCart.find((object) => object.id === event.id) === undefined || false?
-                <button data-key={event.id} onClick={handleBuyClick} className="px-2 py-1 bg-blue-600 text-white rounded-md active:bg-blue-200 active:text-black">
-                  Buy 1 ticket immediately
-                </button>
-                : 
-                shoppingCart.find((object) => object.id === event.id)?.maxReached === true ?
-                <button data-key={event.id} onClick={handleBuyClick} className="px-2 py-1 bg-red-600 text-white rounded-md active:bg-red-500 active:text-black animate-bounce">
-                  Max. Ticket reached
-                </button>
-                  :<button data-key={event.id} onClick={handleBuyClick} className="px-2 py-1 bg-blue-600 text-white rounded-md active:bg-blue-200 active:text-black">
-                  Buy 1 ticket immediately
-                </button>
-                }
+                {shoppingCart.find((object) => object.id === event.id) ===
+                  undefined || false ? (
+                  <button
+                    data-key={event.id}
+                    onClick={handleBuyClick}
+                    className="px-2 py-1 bg-blue-600 text-white rounded-md active:bg-blue-200 active:text-black"
+                  >
+                    Buy 1 ticket immediately
+                  </button>
+                ) : shoppingCart.find((object) => object.id === event.id)
+                    ?.maxReached === true ? (
+                  <button
+                    data-key={event.id}
+                    onClick={handleBuyClick}
+                    className="px-2 py-1 bg-red-600 text-white rounded-md active:bg-red-500 active:text-black animate-bounce"
+                  >
+                    Max. Ticket reached
+                  </button>
+                ) : (
+                  <button
+                    data-key={event.id}
+                    onClick={handleBuyClick}
+                    className="px-2 py-1 bg-blue-600 text-white rounded-md active:bg-blue-200 active:text-black"
+                  >
+                    Buy 1 ticket immediately
+                  </button>
+                )}
                 <Link to={`/products/${event.id}`}>
                   <button className="px-2 py-1 bg-blue-600 text-white rounded-md">
                     Details
--- src/ShoppingPage/index.tsx
+++ src/ShoppingPage/index.tsx
@@ -1,20 +1,68 @@
 import React from "react";
-import { AllProps, ShoppingCart } from "../eventsInterface";
+import { ShoppingCart } from "../eventsInterface";
 import Footer from "../Footer";
 import Header from "../Header";
+import removeimage from "../delete.svg";
 
 export default function ShoppingPage(props: {
-    shoppingCart: ShoppingCart[];
-    onIncrementClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    onDecrementClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-})   {
-    const { shoppingCart, onIncrementClick, onDecrementClick} = props;
-    return (
-        <>
-        <Header shoppingCart={shoppingCart} onIncrementClick={onIncrementClick} onDecrementClick={onDecrementClick} />
-        <h1>Shopping Page</h1>
-        <Footer />
-        </>
-
-    )
+  shoppingCart: ShoppingCart[];
+  onIncrementClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+  onDecrementClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+  onRemoveClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}) {
+  const { shoppingCart, onIncrementClick, onDecrementClick, onRemoveClick } =
+    props;
+    
+  return (
+    <>
+      <Header
+        shoppingCart={shoppingCart}
+        onIncrementClick={onIncrementClick}
+        onDecrementClick={onDecrementClick}
+        onRemoveClick={(e) => onRemoveClick(e)}
+      />
+      <h1 className="text-center">Overview of reserved products</h1>
+      <section className="flex items-center justify-center">
+        {shoppingCart.length > 0 ? (
+          shoppingCart.map((product) => (
+            <div key={product.id} data-id={product.id}>
+              <div>
+                <img src={product.image} alt="product"></img>
+                <p>{product.name}</p>
+              </div>
+              <div>
+                <input
+                title="Input Numbers"
+                  type="number"
+                  className="focus:outline-0"
+                  min={1}
+                  max={product.maxTickets}
+                />
+                <button className="bg-slate-200 px-2 border-2 border-gray-100 rounded-xl">
+                  +
+                </button>
+                <button className="bg-slate-200 px-2 border-2 border-gray-100 rounded-xl">
+                  -
+                </button>
+                <button className="bg-slate-200 px-2 border-2 border-gray-100 rounded-xl relative top-1">
+                  <img
+                    className="h-5 filter"
+                    src={removeimage}
+                    alt="remove product"
+                  />
+                </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h4>Your shopping Cart is empty</h4>
+        )}
+      </section>
+      <Footer />
+    </>
+  );
 }
