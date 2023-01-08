f5e0bd3dc49d7e93733e81299ebd135a50d56b06
Serdar Özdemir <soezdemir3423@gmail.com>
2023-01-08 21:17:41 +0100

    update design, add github link

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
-    return(
-        <footer>
-            Boom
-        </footer>
-    )
+  return (
+    <footer className="text-center py-5 bg-blue-400 fixed bottom-0 w-full">
+      <h6 className="hover:text-3xl hover:text-red-900 hover:animate-ping">
+        <a href="https://github.com/Soezdemir23">Github</a>
+      </h6>
+    </footer>
+  );
 }
