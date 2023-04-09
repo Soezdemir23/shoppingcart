36e9862910cb66a076894d33dee5b6174338cb16
Serdar Özdemir <soezdemir3423@gmail.com>
2023-04-09 12:05:31 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import "./font/Jellee-Bold.ttf";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
